b7a6566607de8fa285388b9eb33b491e417d29cd
Lê  Hoàng Vũ <68913@thegioididong.com>
2024-03-02 16:08:47 +0700

    adding assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import ButtonGradient from './assets/svg/ButtonGradient';
 
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <ButtonGradient />
+    </>
   )
 }
 
